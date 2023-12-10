50f1ec018a73ace5850d8b386a073dccb8beb56c
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-12-10 21:04:01 +0330

    Modify: settings

--- capsule/capsule/settings.py
+++ capsule/capsule/settings.py
@@ -15,14 +15,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv("SECRET_KEY"))
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", True)
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED", "https://*.*").split(",")
 
 # Application definition
 
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "capsule.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / os.getenv("SQLITE_PATH", "db.sqlite3"),
     }
 }
 
@@ -132,13 +132,13 @@ LANGUAGES = [
     ("fa", _("Persian")),
 ]
 
-LOCALE_PATHS = os.getenv("LOCALE_PATHS").split(",")
+LOCALE_PATHS = [os.getenv("LOCALE_PATHS", BASE_DIR / "locale")]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = str(os.getenv("STATIC_ROOT"))
+STATIC_ROOT = os.getenv("STATIC_ROOT", "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
@@ -146,7 +146,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Media files
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = str(os.getenv("MEDIA_ROOT"))
+MEDIA_ROOT = os.getenv("MEDIA_ROOT", "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -158,7 +158,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_USE_TLS = True
-EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
-EMAIL_HOST_USER = str(os.getenv("EMAIL_HOST_USER"))
-EMAIL_HOST_PASSWORD = str(os.getenv("EMAIL_HOST_PASSWORD"))
-DEFAULT_FROM_EMAIL = str(os.getenv("EMAIL_HOST_USER"))
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
